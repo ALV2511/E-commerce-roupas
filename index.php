6b1643c174a1f113537b0b2f90a53f4eb42518be
ALV2511 <alvarouprodrigues@gmail.com>
2026-07-22 19:54:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $host = 'aws-0-us-east-2.pooler.supabase.com'; // Altere para a região da sua c
 $port = '6543';                                // Porta oficial do Supabase Transaction Pooler
 $db   = 'postgres';
 $user = 'postgres.rtzlswsbywvqpeynzfqt';        // Formato correto do usuário: postgres.ID_DO_PROJETO
-$pass = 'SUA_SENHA_AQUI';                      // ⚠️ COLOQUE AQUI A SENHA DO SEU BANCO NO SUPABASE
+$pass = 'K9#mx!L82$vP@qZ7';                      // ⚠️ COLOQUE AQUI A SENHA DO SEU BANCO NO SUPABASE
 
 $dsn = "pgsql:host=$host;port=$port;dbname=$db;";
 
